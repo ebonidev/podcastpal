af29cc9bcb59f5efc15cf19d2a546e48eb5afb61
Eboni E <14114730+ebonidev@users.noreply.github.com>
2023-08-20 09:56:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-%%writefile /content/podcast/app.py
+%%writefile /content/podcast/podcast_frontend.py
 import streamlit as st
 import modal
 import json
@@ -43,11 +43,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'])
+            st.write(podcast_info['podcast_guest']['name'])
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"])
+            st.write(podcast_info["podcast_guest"]['summary'])
 
         # Display the five key moments
         st.subheader("Key Moments")
@@ -91,11 +91,11 @@ def main():
 
         with col3:
             st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'])
+            st.write(podcast_info['podcast_guest']['name'])
 
         with col4:
             st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"])
+            st.write(podcast_info["podcast_guest"]['summary'])
 
         # Display the five key moments
         st.subheader("Key Moments")
